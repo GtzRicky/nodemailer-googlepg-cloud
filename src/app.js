82514bd28d63b1110a4c369a9ec38ad2763ee15d
unknown <ricardogusar93@hotmail.com>
2021-10-08 02:04:13 -0500

    added routes to stripe and multer

--- src/app.js
+++ src/app.js
@@ -1,20 +1,37 @@
+require("dotenv").config();
 const express = require('express');
 const { urlencoded } = require('express');
 const sendMail = require('./utils/nodemailer');
 const cors = require('cors');
+const path = require('path');
+const ejs = require('ejs');
+const multer = require('multer');
+const { Auth } = require('googleapis');
+const { createDecipheriv } = require("crypto");
+const upload = multer({dest: '../public'})
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-app.post("/enviar-correo", async (req, res, next) => {
-    try {
-        const {subject, text, to} = req.body;
+// RUTAS DESDE APP --> // ROUTES>CONTROLLERS>SERVICES...
 
+
+// NODEMAILER
+app.post("/enviar-correo", async (req, res, next) => {
+
+    const {subject, to} = req.body;
+    try {
+        const pathTemplate= path.resolve("src", "views/email-templates", "bienvenida.html");
+
+        const template = await ejs.renderFile(pathTemplate)
+        
         const options = {
             subject,
-            text,
+            html: template,
             to,
             from: 'Mirai <patito2123htp84@gmail.com>'
         };
@@ -23,8 +40,73 @@ app.post("/enviar-correo", async (req, res, next) => {
     
         res.json(response);
     } catch (error) {
+        console.log(error);
+    }
+});
+
+// MULTER
+
+app.post("/subir-archivo", upload.single('file'), (req, res, next) => {
+    res.json({
+        message: "El archivo ha sido cargado en el servidor"
+    });
+});
+
+
+// STRIPE
+
+app.post("/customer", async (req, res, next) => {
+    //Crear cliente
+    try {
+    const { name, email, city, country, line1, line2, postal_code, state } = req.body;
+
+    const customer = await stripe.customers.create({
+        name,
+        email,
+        address: {
+            city,
+            country,
+            line1,
+            line2,
+            postal_code,
+            state
+        },
+    });
+
+    res.json(customer);
+    } catch (error) {
+        next(error);
+}
+});
+
+
+app.post("/customer/:id/payment-method", async (req,res, next) => {
+    const { id: customerId } = req.params;
+    const { number, exp_month, exp_year, cvc } = req.body;
+    //Crear método de pago
+    try {
+        const paymentMethod = await stripe.paymentMethod.create({
+            type: 'card',
+            card: {
+                number,
+                exp_month,
+                exp_year,
+                cvc
+            },
+        });
+
+        const attachPaymentMethod = await stripe.attachPaymentMethod({
+            cost           
+
+        })
+        res.json(paymentMethod);
+    } catch (error) {
         next(error);
     }
+});
+
+app.post("/customer/:id/payment-method", (req,res, next) => {
+    //Modificar método de pago
 });
 
 module.exports = app;
