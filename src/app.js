6b6a5c5a424bc49b678080fd6fd7585c5421922b
unknown <ricardogusar93@hotmail.com>
2021-10-12 01:06:43 -0500

    added new config 2 1am

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
-require("dotenv").config();
 const express = require('express');
 const { urlencoded } = require('express');
 const sendMail = require('./utils/nodemailer');
 const cors = require('cors');
-const path = require('path');
-const ejs = require('ejs');
-const multer = require('multer');
-const { Auth } = require('googleapis');
-const { createDecipheriv } = require("crypto");
-const upload = multer({dest: '../public'})
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
-
 
 const app = express();
 app.use(cors());
@@ -28,7 +19,7 @@ app.post("/enviar-correo", async (req, res, next) => {
     try {
         // const pathTemplate= path.resolve("src", "views/email-templates", "bienvenida.html");
 
-        // const template = await ejs.renderFile(pathTemplate)
+        // const template = await ejs.renderFile(pathTemplate, {title: "Hola mundo"});
         
         const options = {
             subject,
@@ -41,73 +32,86 @@ app.post("/enviar-correo", async (req, res, next) => {
     
         res.json(response);
     } catch (error) {
-        next(error);
+        console.log(error);
     }
-});
-
-// MULTER -> Manejo de archivos y almacenamiento en servidores -> AWS AZURE
-
-app.post("/subir-archivo", upload.single('file'), (req, res, next) => {
-    res.json({
-        message: "El archivo ha sido cargado en el servidor"
-    });
-});
-
-
-// STRIPE
-
-app.post("/customer", async (req, res, next) => {
-    //Crear cliente
-    try {
-    const { name, email, city, country, line1, line2, postal_code, state } = req.body;
-
-    const customer = await stripe.customers.create({
-        name,
-        email,
-        address: {
-            city,
-            country,
-            line1,
-            line2,
-            postal_code,
-            state
-        },
-    });
-
-    res.json(customer);
-    } catch (error) {
-        next(error);
-}
-});
-
-
-app.post("/customer/:id/payment-method", async (req,res, next) => {
-    const { id: customerId } = req.params;
-    const { number, exp_month, exp_year, cvc } = req.body;
-    //Crear método de pago
-    try {
-        const paymentMethod = await stripe.paymentMethod.create({
-            type: 'card',
-            card: {
-                number,
-                exp_month,
-                exp_year,
-                cvc
-            },
-        });
-
-        const attachPaymentMethod = await stripe.attachPaymentMethod({
-            cost           
-
-        })
-        res.json(paymentMethod);
-    } catch (error) {
-        next(error);
-    }
-});
-
-app.post("/customer/:id/payment-method", (req,res, next) => {
-    //Modificar método de pago
 });
 
 module.exports = app;
+
+//IMPORTS
+
+// require("dotenv").config();
+// const path = require('path');
+// const ejs = require('ejs');
+// const multer = require('multer');
+// const { Auth } = require('googleapis');
+// const { createDecipheriv } = require("crypto");
+// const upload = multer({dest: '../public'})
+// const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
+
+// MULTER -> Manejo de archivos y almacenamiento en servidores -> AWS AZURE
+
+// app.post("/subir-archivo", upload.single('file'), (req, res, next) => {
+//     res.json({
+//         message: "El archivo ha sido cargado en el servidor"
+//     });
+// });
+
+
+// // STRIPE
+
+// app.post("/customer", async (req, res, next) => {
+//     //Crear cliente
+//     try {
+//     const { name, email, city, country, line1, line2, postal_code, state } = req.body;
+
+//     const customer = await stripe.customers.create({
+//         name,
+//         email,
+//         address: {
+//             city,
+//             country,
+//             line1,
+//             line2,
+//             postal_code,
+//             state
+//         },
+//     });
+
+//     res.json(customer);
+//     } catch (error) {
+//         next(error);
+// }
+// });
+
+
+// app.post("/customer/:id/payment-method", async (req,res, next) => {
+//     const { id: customerId } = req.params;
+//     const { number, exp_month, exp_year, cvc } = req.body;
+//     //Crear método de pago
+//     try {
+//         const paymentMethod = await stripe.paymentMethod.create({
+//             type: 'card',
+//             card: {
+//                 number,
+//                 exp_month,
+//                 exp_year,
+//                 cvc
+//             },
+//         });
+
+//         const attachPaymentMethod = await stripe.attachPaymentMethod({
+//             cost           
+
+//         })
+//         res.json(paymentMethod);
+//     } catch (error) {
+//         next(error);
+//     }
+// });
+
+// app.post("/customer/:id/payment-method", (req,res, next) => {
+//     //Modificar método de pago
+// });
+
